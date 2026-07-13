d82168d2718b96669c523b8f5f657a16931f4860
Matthew Cool <matthewmichaelcool@gmail.com>
2026-07-12 21:02:09 -0700

    3D Solar System Simulation

--- components/about-page-component.js
+++
@@ -1,9 +0,0 @@
-export default {
-  name: 'about-page-component',
-  template: /* html */ `
-    <section class="container py-4">
-      <h1>About</h1>
-      <p>This app is a beginner prototype for exploring planets and landing sites in the solar system. It shows how a simple website can use CSV data to create an interactive, space-themed experience.</p>
-    </section>
-  `,
-};
